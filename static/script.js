2d650a7d354e36564b03a7459ea49bdb01ad8425
yashwanth0512 <97393387+yashwanth0512@users.noreply.github.com>
2023-07-26 09:59:09 +0530

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,70 @@
+// function displayFileName() {
+//     const fileInput = document.getElementById('upload');
+//     const fileName = fileInput.files[0].name;
+//     const fileNameElement = document.getElementById('file-name');
+//     fileNameElement.textContent = `Uploaded File: ${fileName}`;
+//   }
+  
+// function displayFileName() {
+//   const fileInput = document.getElementById('upload');
+//   const fileName = document.getElementById('file-name');
+//   fileName.textContent = fileInput.files[0].name;
+// }
+function displayFileName() {
+  const fileInput = document.getElementById('upload');
+  const fileNameElement = document.getElementById('file-name');
+  const uploadedImageElement = document.getElementById('uploaded-image');
+
+  const file = fileInput.files[0];
+  fileNameElement.textContent = file.name;
+
+  const reader = new FileReader();
+  reader.onload = function(e) {
+    uploadedImageElement.src = e.target.result;
+  };
+  reader.readAsDataURL(file);
+}
+function convertImage() {
+  const form = document.getElementById('upload-form');
+  const formData = new FormData(form);
+  
+  fetch('', {
+    method: 'POST',
+    body: formData,
+    headers: {
+      'X-CSRFToken': '{{ csrf_token }}',
+    }
+  })
+  .then(response => response.blob())
+  .then(blob => {
+    const imageUrl = URL.createObjectURL(blob);
+    const denoisedImageContainer = document.getElementById('denoised-image-container');
+    denoisedImageContainer.innerHTML = `<img src="${imageUrl}" alt="Denoised Image">`;
+  })
+  .catch(error => console.error('Error:', error));
+}
+
+
+function convertImage() {
+  const fileInput = document.getElementById('upload');
+  const file = fileInput.files[0];
+
+  const formData = new FormData();
+  formData.append('image', file);
+
+  fetch('', {
+    method: 'POST',
+    body: formData,
+  })
+    .then(response => response.blob())
+    .then(blob => {
+      const outputContainer = document.getElementById('output-container');
+      outputContainer.innerHTML = '';
+
+      const img = document.createElement('img');
+      img.src = URL.createObjectURL(blob);
+      img.classList.add('output-image');
+      outputContainer.appendChild(img);
+    })
+    .catch(error => console.log(error));
+}
